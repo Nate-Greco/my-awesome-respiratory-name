c9981a5cb06b3c8bba40c1b791c2a464ce34c3af
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2025-04-06 11:22:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-document.getElemenstByClassName("switch")[0].onclick = () => {
-  if (!document.getElementById("switchy").checked) {
-    var rules = document.styleSheets[0].rules
-    console.log(rules)
-    console.log(document.styleSheets[0])
-    for (var i = 0; i < rules.length; i++) {
-      if (rules[i].selectorText == "p") {
-        rules[i].style.color = "purple"
-      } else if (rules[i].selectorText == "h1") {
-        rules[i].style.color = "purple"
-      } else if (rules[i].selectorText == "body") {
-        rules[i].style.backgroundColor = "#f0dc00"
+while (true) {
+  try {
+    document.getElementById("switchy").onclick = () => {
+      if (!document.getElementById("switchy").checked) {
+        var rules = document.styleSheets[0].rules
+        console.log(rules)
+        console.log(document.styleSheets[0])
+        for (var i = 0; i < rules.length; i++) {
+          if (rules[i].selectorText == "p") {
+            rules[i].style.color = "purple"
+          } else if (rules[i].selectorText == "h1") {
+            rules[i].style.color = "purple"
+          } else if (rules[i].selectorText == "body") {
+            rules[i].style.backgroundColor = "#f0dc00"
+          }
+        }
+      } else {
+        var rules = document.styleSheets[0].rules
+        for (var i = 0; i < rules.length; i++) {
+          if (rules[i].selectorText == "p") {
+            rules[i].style.color = "#f0dc00"
+          } else if (rules[i].selectorText == "h1") {
+            rules[i].style.color = "#f0dc00"
+          } else if (rules[i].selectorText == "body") {
+            rules[i].style.backgroundColor = "purple"
+          }
+        }
       }
     }
-  } else {
-    var rules = document.styleSheets[0].rules
-    for (var i = 0; i < rules.length; i++) {
-      if (rules[i].selectorText == "p") {
-        rules[i].style.color = "#f0dc00"
-      } else if (rules[i].selectorText == "h1") {
-        rules[i].style.color = "#f0dc00"
-      } else if (rules[i].selectorText == "body") {
-        rules[i].style.backgroundColor = "purple"
-      }
-    }
+    break
+  } catch (error) {
+    console.log("BAD")
+    console.log(error)
   }
 }
